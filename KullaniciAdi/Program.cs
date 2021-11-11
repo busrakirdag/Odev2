1acd6499d05cb614fa3005f9eda6c2dc6fca1742
YOGA <you@example.com>
2021-11-11 19:13:42 +0300

    Add project files.

---
+++ KullaniciAdi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace KullaniciAdi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kullaniciadi, sifre;
+            Console.WriteLine("Kullanıcı Adı Giriniz:");
+            kullaniciadi = Console.ReadLine();
+
+            Console.WriteLine("Şifrenizi Giriniz:");
+            sifre = Console.ReadLine();
+
+
+            if (kullaniciadi == "admin" && sifre == "123")
+            {
+                Console.WriteLine("Hoşgeldiniz..");
+            }
+            else {
+                int i = 0;
+                while (i < 3)
+                {
+
+                    Console.WriteLine("Hatalı giriş yaptınız..");
+                    Console.WriteLine("Kullanıcı Adı Giriniz:");
+                    kullaniciadi = Console.ReadLine();
+                    Console.WriteLine("Şifrenizi Giriniz:");
+                    sifre = Console.ReadLine();
+                    if (kullaniciadi != "admin" && sifre != "123")
+                    {
+                        i++;    
+                    }
+                    
+                }
+                
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ OgrenciOrtalamasi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace OgrenciOrtalamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Öğrenci Sayısını Giriniz:");
+            int ogrenciSayisi = Convert.ToInt32(Console.ReadLine());
+            double ortalama, genelOrt;
+            double toplam = 0;
+            double endusukOrt = 100;
+            double enyuksekOrt = -1;
+            for (int i = 0; i < ogrenciSayisi; i++)
+            {
+                Console.WriteLine($"Lütfen {i+1}. Öğrencinin Vize Notunu Giriniz");
+                int vize = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"Lütfen {i+1}. Öğrencinin Final Notunu Giriniz");
+                int final = Convert.ToInt32(Console.ReadLine());
+
+                ortalama = (vize * 0.4) + (final * 0.6);
+                Console.WriteLine($"{i+1}. Öğrenci Ortalaması:{ortalama}");
+
+                if (ortalama > enyuksekOrt)
+                {
+                    enyuksekOrt = ortalama;
+                }
+                if (ortalama < endusukOrt)
+                {
+                    endusukOrt = ortalama;
+                }
+                toplam += ortalama;
+                
+
+            }
+            genelOrt = toplam / ogrenciSayisi;
+            Console.WriteLine($"En Düşük Öğrenci Ortalaması: {endusukOrt}");
+            Console.WriteLine($"En Yüksek Öğrenci Ortalaması: {enyuksekOrt}");
+            Console.WriteLine($"Genel Öğrenci Ortalaması: {genelOrt}");
+            Console.ReadKey();
+        }
+    }
+}
